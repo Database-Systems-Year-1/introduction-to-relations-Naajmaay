cca07aeb616ed442062a17cef70a78c5d7e4fe85
Najma chueb <chueb.najma@strathmore.edu>
2024-09-10 12:12:36 +0300

    Add files via upload

---
+++ student.sql
@@ -0,0 +1,16 @@
+create table student (
+jina VARCHAR(255),
+age INT ,
+course VARCHAR(255)
+);
+
+insert into student (jina, age, course)
+VALUES ('Najma',20,'BBIT')
+
+SELECT* FROM STUDENT
+
+
+
+
+
+
